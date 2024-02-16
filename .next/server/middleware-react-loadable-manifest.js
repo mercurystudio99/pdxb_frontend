3515c0ffd14b57330ce39e504945be33e91780b4
mercurystudio99 <mercurystudio99@gmail.com>
2024-02-15 23:32:23 -0800

    remove unnecessary folder

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"..\\src\\Components\\User\\UserAddProperty.jsx -> ../AdminLayout/VerticleLayout.jsx":{"id":"..\\src\\Components\\User\\UserAddProperty.jsx -> ../AdminLayout/VerticleLayout.jsx","files":["static/chunks/src_Components_AdminLayout_VerticleLayout_jsx.js"]}}
